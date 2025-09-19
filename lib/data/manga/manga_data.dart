590e8ff980fed5719bf517eae23a0b85ef20b32f
FaizNation <novaatalagrab@gmail.com>
2025-09-19 23:29:15 +0700

    refactoring code

--- lib/data/manga/manga_data.dart
+++ lib/data/manga/manga_data.dart
@@ -7,8 +7,7 @@ List<Comic> mangaList = [
     title: "One Piece",
     author: "Eiichiro Oda",
     description: "Petualangan bajak laut mencari harta karun legendaris.",
-    coverImage:
-        "https://thumbnail.komiku.org/uploads/manga/komik-one-piece-indo/manga_thumbnail-Komik-One-Piece.jpg?",
+    coverImage: "https://thumbnail.komiku.org/uploads/manga/komik-one-piece-indo/manga_thumbnail-Komik-One-Piece.jpg?",
     rating: 4.9,
     genres: ["Action", "Adventure", "Fantasy"],
     status: "Ongoing",
@@ -19,10 +18,8 @@ List<Comic> mangaList = [
     id: "ds001",
     title: "Demon Slayer",
     author: "Koyoharu Gotouge",
-    description:
-        "Perjalanan Tanjiro menjadi pemburu iblis untuk menyelamatkan adiknya.",
-    coverImage:
-        "https://upload.wikimedia.org/wikipedia/en/0/09/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg",
+    description: "Perjalanan Tanjiro menjadi pemburu iblis untuk menyelamatkan adiknya.",
+    coverImage: "https://upload.wikimedia.org/wikipedia/en/0/09/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg",
     rating: 4.8,
     genres: ["Action", "Dark Fantasy", "Supernatural"],
     status: "competed",
@@ -33,10 +30,8 @@ List<Comic> mangaList = [
     id: "jjk001",
     title: "Jujutsu Kaisen",
     author: "Gege Akutami",
-    description:
-        "Cerita tentang siswa SMA yang terlibat dalam dunia kutukan dan jujutsu.",
-    coverImage:
-        "https://upload.wikimedia.org/wikipedia/en/4/46/Jujutsu_kaisen.jpg",
+    description: "Cerita tentang siswa SMA yang terlibat dalam dunia kutukan dan jujutsu.",
+    coverImage: "https://upload.wikimedia.org/wikipedia/en/4/46/Jujutsu_kaisen.jpg",
     rating: 4.8,
     genres: ["Action", "Supernatural", "Horror"],
     status: "competed",
@@ -47,10 +42,8 @@ List<Comic> mangaList = [
     id: "bnh001",
     title: "Boku no Hero Academia",
     author: "Kohei Horikoshi",
-    description:
-        "Kisah seorang anak tanpa kekuatan di dunia penuh pahlawan super.",
-    coverImage:
-        "https://thumbnail.komiku.org/uploads/manga/boku-no-hero-academia-indonesia/manga_thumbnail-Komik-Boku-no-Hero-Academia.jpg",
+    description: "Kisah seorang anak tanpa kekuatan di dunia penuh pahlawan super.",
+    coverImage: "https://thumbnail.komiku.org/uploads/manga/boku-no-hero-academia-indonesia/manga_thumbnail-Komik-Boku-no-Hero-Academia.jpg",
     rating: 4.8,
     genres: ["Action", "Superhero", "School"],
     status: "competed",
