5f21a73e99b11304a5ab417b1dc51a26f2ce8c42
FaizNation <novaatalagrab@gmail.com>
2025-10-11 08:49:25 +0700

    feat: update validation messages

--- lib/utils/logout_dialog.dart
+++ lib/utils/logout_dialog.dart
@@ -4,17 +4,17 @@ Future<bool?> showLogoutDialog(BuildContext context) {
   return showDialog<bool>(
     context: context,
     builder: (context) => AlertDialog(
-      title: const Text("Logout"),
-      content: const Text("Apakah kamu yakin ingin logout?"),
+      title: const Text("Hold up!"),
+      content: const Text("You sure you wanna sign out?"),
       actions: [
         TextButton(
           onPressed: () => Navigator.of(context).pop(false),
-          child: const Text("Batal"),
+          child: const Text("Cancel bro"),
         ),
         ElevatedButton(
           onPressed: () => Navigator.of(context).pop(true),
           style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
-          child: const Text("Logout"),
+          child: const Text("I’m out", style: TextStyle(color: Colors.white)),
         ),
       ],
     ),
--- lib/utils/showexit.dart
+++ lib/utils/showexit.dart
@@ -4,16 +4,16 @@ Future<bool> showExitConfirmationDialog(BuildContext context) async {
   final result = await showDialog<bool>(
     context: context,
     builder: (context) => AlertDialog(
-      title: const Text("Konfirmasi"),
-      content: const Text("Yakin mau kembali?"),
+      title: const Text("Hold up!"),
+      content: const Text("Are u sure you wanna go back?"),
       actions: [
         TextButton(
           onPressed: () => Navigator.of(context).pop(false),
-          child: const Text("Batal"),
+          child: const Text("Nope"),
         ),
         TextButton(
           onPressed: () => Navigator.of(context).pop(true),
-          child: const Text("Ya"),
+          child: const Text("Yup"),
         ),
       ],
     ),
